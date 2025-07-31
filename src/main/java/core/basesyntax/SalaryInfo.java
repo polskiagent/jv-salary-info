720e9567dc15164ea1a608019ad3102b6b93d5b7
Piotr Seredynski <sersoft14@gmail.com>
2025-07-31 07:43:41 +0200

    add for

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -8,15 +8,26 @@ public class SalaryInfo {
 
         LocalDate from = LocalDate.parse(dateFrom, FORMATTER);
         LocalDate to = LocalDate.parse(dateTo, FORMATTER);
+        StringBuilder tak = new StringBuilder();
+        tak.append("Report for period ").append(dateFrom).append(" - ").append(dateTo).append(System.lineSeparator());
 
         for (int i = 0; i < names.length; i++) {
             int salary = 0;
-            for (int j = 0; j < data.length; j++)
+
+            for (int j = 0; j < data.length; j++) {
+                String[] parts = data[j].split(" ");
+                LocalDate date = LocalDate.parse(parts[0], FORMATTER);
+
+                if (!date.isBefore(from) && !date.isAfter(to) && parts[1].equals(names[i])) {
+                    salary += Integer.parseInt(parts[2]) * Integer.parseInt(parts[3]);
+
+                }
+            }
+            tak.append(names[i]).append(" - ").append(salary).append(System.lineSeparator());
+
         }
+        return tak.toString();
     }
-
-
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-
 
 }
