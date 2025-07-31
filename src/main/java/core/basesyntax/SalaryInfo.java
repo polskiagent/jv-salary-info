4b284496cfa8be16803783212aeb02b68e37f9e2
Piotr Seredynski <sersoft14@gmail.com>
2025-07-31 07:50:57 +0200

    fix bugs

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,12 +4,15 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
 
         LocalDate from = LocalDate.parse(dateFrom, FORMATTER);
         LocalDate to = LocalDate.parse(dateTo, FORMATTER);
         StringBuilder tak = new StringBuilder();
-        tak.append("Report for period ").append(dateFrom).append(" - ").append(dateTo).append(System.lineSeparator());
+        tak.append("Report for period ").append(dateFrom).append(" - ").append(dateTo)
+                .append(System.lineSeparator());
 
         for (int i = 0; i < names.length; i++) {
             int salary = 0;
@@ -20,14 +23,10 @@ public class SalaryInfo {
 
                 if (!date.isBefore(from) && !date.isAfter(to) && parts[1].equals(names[i])) {
                     salary += Integer.parseInt(parts[2]) * Integer.parseInt(parts[3]);
-
                 }
             }
             tak.append(names[i]).append(" - ").append(salary).append(System.lineSeparator());
-
         }
         return tak.toString();
     }
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-
 }
